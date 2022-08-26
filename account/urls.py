42c86e579d177f6793360a2f29c5e8b555d21904
Zaki <87921831+Starlordhunter@users.noreply.github.com>
2022-08-26 11:47:17 +0800

    Update urls.py
    Add ClassInfo url to setup the path for the view

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
-from .views import LoginAPI, MeView, get_all_user_list
+from .views import LoginAPI, MeView, get_all_user_list,ClassInfo
 from knox.urls import views as knoxviews
+from . import views
+
 
 
 urlpatterns = [
-    
     path('log-in/',LoginAPI.as_view() ,name='log-in'),
     path('log-out/',knoxviews.LogoutView.as_view() ,name='log-out'),
     path('me/', MeView.as_view(), name='me'),
     path('users/', get_all_user_list), 
+    path('teacher/',ClassInfo.as_view(),name='classInfo')
+
+    # path('',views.index,name='index'),
     
 ]
