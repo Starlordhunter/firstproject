c426ea348e8a42638ac5ebed3ad0ec60617cf289
Zaki <87921831+Starlordhunter@users.noreply.github.com>
2022-08-30 09:43:39 +0800

    Update urls.py
    latest url

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import LoginAPI, MeView, get_all_user_list,get_class_info
+from .views import LoginAPI, MeView, get_all_user_list,ClassList,StudentsList
 from knox.urls import views as knoxviews
 from . import views
 
@@ -10,8 +10,10 @@ urlpatterns = [
     path('log-in/',LoginAPI.as_view() ,name='log-in'),
     path('log-out/',knoxviews.LogoutView.as_view() ,name='log-out'),
     path('me/', MeView.as_view(), name='me'),
-    path('users/', get_all_user_list), 
-    path('teacher/',get_class_info)
+    path('users/list', get_all_user_list), 
+    # path('school/list',),
+    path('class/list',ClassList.as_view(),name='classes'),
+    path('students/list',StudentsList.as_view(),name='students')
 
     # path('',views.index,name='index'),
     
