e093826c26bde5a46b5a4d306d3633e5eec9bb26
Zaki <87921831+Starlordhunter@users.noreply.github.com>
2022-09-02 11:21:21 +0800

    update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import LoginAPI, MeView, get_all_user_list,ClassList,StudentsList,SchoolList,TeacherList
+from .views import LoginAPI, MeView, get_all_user_list,ClassList,StudentsList,SchoolList,TeacherList,PrincipalList
 from knox.urls import views as knoxviews
 from . import views
 
@@ -14,7 +14,8 @@ urlpatterns = [
     path('school/list',SchoolList.as_view(),name='school'),
     path('class/list',ClassList.as_view(),name='classes'),
     path('teacher/list',TeacherList.as_view(),name='teachers'),
-    path('students/list',StudentsList.as_view(),name='students')
+    path('students/list',StudentsList.as_view(),name='students'),
+    path('principal/list',PrincipalList.as_view(),name='principal')
 
     # path('',views.index,name='index'),
     
