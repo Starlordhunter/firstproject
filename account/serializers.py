48326de63f851d11e0d69191a310efa97e0253cd
Zaki <87921831+Starlordhunter@users.noreply.github.com>
2022-09-02 11:20:50 +0800

    Update serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -1,10 +1,11 @@
 from asyncio.windows_events import NULL
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 # from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
 
 # from core.serializers import StaffSerializer
-from .models import Classes, School, Student, Teacher, User, Profile
+from .models import Classes, School, Student, Subject, Teacher, User, Profile
 
 
 #ProfileSerializer
@@ -33,7 +34,7 @@ class GetUserSerializer(serializers.ModelSerializer):
     
     def get_profile(self ,obj):
         try :
-            profile = obj.profile
+            profile = obj
             return ProfileSerializer(profile).data
         except :
             return None
@@ -47,8 +48,15 @@ class GetUserSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer): 
     
-    # profile = serializers.SerializerMethodField()
+    profile = serializers.SerializerMethodField()
     
+    def get_profile(self ,obj):
+        try :
+            profile = Profile.objects.get(profile_user__in=obj)
+            print(profile)
+            return ProfileSerializer(profile).data
+        except :
+            return None
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 
@@ -72,14 +80,22 @@ class SchoolSerializer(serializers.ModelSerializer):
         model = School
         fields = '__all__'
 
+#SubjectSerializer
+class SubjectSerializer(serializers.ModelSerializer):
+    subject_name = serializers.CharField(max_length=20)
+    class Meta:
+        model = Subject
+        fields = ['subject_name']
+
 #StudentSerializer
 class StudentSerializer(serializers.ModelSerializer):
     
     student_name = serializers.CharField(max_length=100)
     student_tel_no = serializers.CharField(max_length=100)
+    subject_taken = SubjectSerializer(many=True)
     class Meta:
         model = Student
-        fields = ['id', 'student_name','student_tel_no','student_address','classes_id','user_id']
+        fields = ['id', 'student_name','student_tel_no','student_address','classes_id','user_id','subject_taken']
 
 #ClassesSerializer
 class ClassesSerializer(serializers.ModelSerializer):
@@ -87,6 +103,7 @@ class ClassesSerializer(serializers.ModelSerializer):
     class_name = serializers.CharField(max_length=100)
     student_in_classes = StudentSerializer(many=True)
     
+
 
     class Meta:
         model = Classes
@@ -99,3 +116,22 @@ class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'
+
+#PrincipalSerializer
+class PrincipalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+#PrincipalListSerializer
+class PrincipalListSerializer(serializers.ModelSerializer):
+    count_of_students = serializers.IntegerField()
+    class_name = serializers.CharField(max_length=100)
+    student_in_classes = StudentSerializer(many=True)
+    teacher_info = TeacherSerializer(many=True)
+    
+
+
+    class Meta:
+        model = Classes
+        fields = ['teacher_info','class_name','count_of_students','student_in_classes']
